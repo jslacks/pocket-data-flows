41d0eee46efc98bec163c6151a1529d125c56944
james-impact <95827023+james-impact@users.noreply.github.com>
2023-06-09 02:51:34 -0400

    Configure custom deployment

--- .aws/src/config/index.ts
+++ .aws/src/config/index.ts
@@ -1,16 +1,16 @@
 const name = 'DataFlows';
 const domainPrefix = 'data-flows';
-const s3BucketPrefix = 'pocket';
+const s3BucketPrefix = 'impact';
 const isDev = process.env.NODE_ENV === 'development';
 const environment = isDev ? 'Dev' : 'Prod';
 const fullEnvironment = isDev ? 'development' : 'production';
 const domain = isDev
-  ? `${domainPrefix}.getpocket.dev`
-  : `${domainPrefix}.readitlater.com`;
+  ? `${domainPrefix}.impactsystems.net`
+  : `${domainPrefix}.impactsystems.net`;
 const graphqlVariant = isDev ? 'development' : 'current';
 const githubConnectionArn = isDev
-  ? 'arn:aws:codestar-connections:us-east-1:410318598490:connection/7426c139-1aa0-49e2-aabc-5aef11092032'
-  : 'arn:aws:codestar-connections:us-east-1:996905175585:connection/5fa5aa2b-a2d2-43e3-ab5a-72ececfc1870';
+  ? 'arn:aws:codestar-connections:us-west-2:480076400879:connection/32316762-9d08-4734-95d4-7043b1119270'
+  : 'arn:aws:codestar-connections:us-west-2:480076400879:connection/32316762-9d08-4734-95d4-7043b1119270';
 const branch = isDev ? 'dev' : 'main';
 
 // Git branch name is used to determine which Prefect project to register the tasks in: dev or prod.
@@ -32,35 +32,16 @@ const prefect = {
     // For queuing large files to disk... https://docs.amazonaws.cn/en_us/AmazonECS/latest/userguide/fargate-task-defs.html#fargate-tasks-storage
     ephemeralStorageSizeInGB: 200,
     dataLearningBucketName: isDev
-      ? 'pocket-data-learning-dev'
-      : 'pocket-data-learning',
+      ? 'impact-virtuoso-maestro'
+      : 'impact-virtuoso-maestro',
+
+
     // To securely inject an environment variable FOO_BAR in the ECS task that executes Prefect Flows, add 'FOO_BAR' to
     // the list below and create Parameters /DataFlows/Prod/FOO_BAR and /DataFlows/Dev/FOO_BAR in Prod and Dev.
-    parameterStoreNames: [
-      'SNOWFLAKE_PRIVATE_KEY',
-      'SNOWFLAKE_ACCOUNT',
-      'SNOWFLAKE_USER',
-      'DBT_CLOUD_TOKEN',
-      'DBT_CLOUD_ACCOUNT_ID',
-      'GCE_KEY',
-      'BRAZE_API_KEY',
-      'BRAZE_REST_ENDPOINT',
-      'SNOWFLAKE_DATA_RETENTION_ROLE',
-      'SNOWFLAKE_DATA_RETENTION_WAREHOUSE',
-      'SNOWFLAKE_DATA_RETENTION_DB',
-      'SNOWFLAKE_DATA_RETENTION_SCHEMA',
-      'SNOWFLAKE_SNOWPLOW_DB',
-      'SNOWFLAKE_SNOWPLOW_SCHEMA',
-      'SNOWFLAKE_RAWDATA_DB',
-      'SNOWFLAKE_RAWDATA_FIREHOSE_SCHEMA',
-      'SNOWFLAKE_SNAPSHOT_DB',
-      'SNOWFLAKE_SNAPSHOT_FIREHOSE_SCHEMA',
-      'POCKET_PUBLISHER_DATABASE_HOST',
-      'POCKET_PUBLISHER_DATABASE_PORT',
-      'POCKET_PUBLISHER_DATABASE_DBNAME',
-      'POCKET_PUBLISHER_DATABASE_USER',
-      'POCKET_PUBLISHER_DATABASE_PASSWORD',
-    ],
+    parameterStoreNames: ['FOO_BAR'],
+
+
+
     // Use the existing 'PocketDataProductReadOnly' policy. It currently only exists in production.
     // @see https://github.com/Pocket/data-shared/blob/main/lib/permissions-stack.ts#L14
     existingPolicies: isDev ? [] : ['PocketDataProductWriteAccess'],
@@ -71,7 +52,7 @@ export const config = {
   name,
   isDev,
   prefix: `${name}-${environment}`,
-  awsRegion: 'us-east-1',
+  awsRegion: 'us-west-2',
   s3BucketPrefix,
   circleCIPrefix: `/${name}/CircleCI/${environment}`,
   shortName: 'DATAFL',
@@ -80,14 +61,14 @@ export const config = {
   domain,
   prefect,
   terraform: {
-    organization: 'Pocket',
+    organization: 'impact-analytics',
   },
   codePipeline: {
     artifactBucketPrefix: `${s3BucketPrefix}-codepipeline`,
     githubConnectionArn,
-    repository: 'pocket/data-flows',
+    repository: 'jslacks/pocket-data-flows',
     branch,
-    codeDeploySnsTopicName: `DataAndLearning-${environment}-ChatBot`,
+    codeDeploySnsTopicName: `DataFlows-${environment}-Notify`,
   },
   graphqlVariant,
   healthCheck: {
--- src/flows/examples/s3_download_flow.py
+++ src/flows/examples/s3_download_flow.py
@@ -5,7 +5,7 @@ from utils.flow import get_flow_name
 
 FLOW_NAME = get_flow_name(__file__)
 
-s3_bucket_source = 'pocket-data-learning-dev'
+s3_bucket_source = 'impact-virtuoso-maestro'
 s3_bucket_key='analytics-modeled-data/parquet/dbt/web_explore_impressions_clicks_by_item_hour/data_0_0_0.snappy.parquet'
 
 with Flow(FLOW_NAME) as flow:
